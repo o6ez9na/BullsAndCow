2a35f672ea2b83e308ae2f3f38cba4b8bb52da96
o6ez9na <kikikippplo@gmail.com>
2023-05-28 14:32:31 +0300

    release

--- src/ui_components/ui/controller/MainMenuController.cpp
+++ src/ui_components/ui/controller/MainMenuController.cpp
@@ -1,15 +1,18 @@
 #include "MainMenuController.h"
 void MainMenuController::UpdateEvents() {
-  std::cout << "Input <next> for continue\n";
+  std::cout << "Input <next> for continue or <exit> for exit =)\n";
   static std::string s;
 
   std::cin >> s;
   if (s == "next") {
     on_write_next->Notify();
+  } else if (s == "exit") {
+    abort();
   } else {
     UpdateEvents();
   }
 }
+
 MainMenuController::MainMenuController() {
   on_write_next = new Subject();
 }
